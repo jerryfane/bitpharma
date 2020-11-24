02a70078c954eea9eae0f7c870f729f3fd5e71f7
AntoBcc <72571923+AntoBcc@users.noreply.github.com>
2020-11-24 15:39:39 +0100

    more features and events

--- prova.sol
+++ prova.sol
@@ -5,16 +5,25 @@ contract bitpharma {
     address doctor;
     address patient;
     address pharmacy;
-
+    
+    // these events will be generated when transaction is mined (and hopefully we will catch them with Python)
+    event Prescribed(string _drug, uint _quantity, uint _maxclaim, uint _purchaseCooldown, uint _daysToExpiration, address _patient);
+    event Transaction(uint _prescrId, uint _quantity); // a purchase has been made, as confirmed by both parties  
+    event Closed(uint _prescrId); // a prescription contract has been closed
+    
+    
     constructor  (address _doctor, address _patient, address _pharmacy) public {
         //to initialize addresses
         doctor = _doctor;
         patient = _patient;
         pharmacy = _pharmacy;
     }
+    
 
     struct prescription {
-        string drug;
+        //N.B: solidity  does not allow for string comparison, need to make sure all strings parameters are lowercased
+        //we could import this contract to lowercase: https://gist.github.com/ottodevs/c43d0a8b4b891ac2da675f825b1d1dbf  
+        string drug; 
         uint quantity;  //total quantity of the drug claimable before prescription expires;
         uint quantity_claimed; //quantity claimed by user in a single purchase
         uint maxclaim; //max amount claimable in single purchase
@@ -22,7 +31,7 @@ contract bitpharma {
         uint last_purchase; //timestamp of latest purchase
         uint expiration; //days from when prescription was issued
         uint status; //status legend: 0-> prescription is issued, 1-> patient confirms purchase,
-        //2-> pharma clossed transaction successfully, 3-> prescription expired before purchase...
+        //2-> pharma closed transaction successfully, 3-> prescription expired before purchase...
 
         //bool exists;
         }
@@ -39,6 +48,7 @@ contract bitpharma {
         uint id = prescriptions.push(prescription(_drug, _quantity, 0, _maxclaim, _purchaseCooldown * 1 days, 0, now + _daysToExpiration * 1 days ,0)) - 1;
         prescription_to_patient[id] = _patient;
         active_prescriptions[_patient]++;
+        emit Prescribed(_drug,_quantity,_maxclaim,_purchaseCooldown,_daysToExpiration,_patient);
     }
 
     function patient_purchasing(uint _prescrId, uint _quantity) external {  //patient can confirm the purchase of a prescription in the array
@@ -70,11 +80,13 @@ contract bitpharma {
         else {
             prescriptions[_prescrId].status = 2;
             active_prescriptions[prescription_to_patient[_prescrId]]--;
+            emit Closed(_prescrId);
         }
         prescriptions[_prescrId].quantity_claimed=0;
         prescriptions[_prescrId].last_purchase=now;
+        emit Transaction(_prescrId, _quantity);
     }
-
+    
 
     function my_prescriptions() external view returns(uint[] memory list_of_myPrescriptions_Ids) {  //return all active prescriptions IDs of a patient
         require(msg.sender==patient, "You can't access prescriptions!");
@@ -90,14 +102,48 @@ contract bitpharma {
         //return result;
     }
     
+    // TO ADD, when we have multiple doctors:
+    // not all doctors can be allowed to access a random patient's prescriptions (that is not respecting their privacy)
+    // Only doctors that are prescribing something to them should be able to do this if they want to. 
+    // Actually, we need to check because I am not sure whether explicit consent from the patient is needed. 
+    function patient_prescriptions(address _patient) external view  returns(uint[] memory list_of_Prescriptions_Ids) {
+        require(msg.sender==doctor, "You can't access prescriptions!");
+        uint[] memory result = new uint[](active_prescriptions[_patient]);
+        uint counter = 0;
+        for (uint i = 0; i < prescriptions.length; i++) {
+            if (prescription_to_patient[i] == _patient && prescriptions[i].status<2) {
+                result[counter] = i;
+                counter++;
+            }
+        }
+        list_of_Prescriptions_Ids = result;
+    }
+    
+    function patient_prescriptions(address _patient, string calldata _drug) external view  returns(uint[] memory list_of_Prescriptions_Ids) {
+        require(msg.sender==doctor, "You can't access prescriptions!");
+        uint[] memory result = new uint[](active_prescriptions[_patient]);
+        uint counter = 0;
+        // note: solidity does not allow for string comparison, but it does compare hashes (everything needs to be lowercased!)
+        for (uint i = 0; i < prescriptions.length; i++) {
+            if (prescription_to_patient[i] == _patient && prescriptions[i].status<2 && 
+            keccak256(bytes(prescriptions[i].drug)) == keccak256(bytes(_drug))) {
+                result[counter] = i;
+                counter++;
+            }
+        }
+        list_of_Prescriptions_Ids = result;
+    }
+    
     
     function prescription_details(uint _prescrId) external view returns(string memory drug, uint quantity_left, uint max_claim, bool can_I_buy, uint time_to_expiration, uint status) {
-        require(msg.sender==prescription_to_patient[_prescrId], "You can't see this prescription!");
+        require(msg.sender==prescription_to_patient[_prescrId] || msg.sender==doctor, "You can't see this prescription!");
         drug = prescriptions[_prescrId].drug;
         quantity_left = prescriptions[_prescrId].quantity;
         max_claim = prescriptions[_prescrId].maxclaim;
         can_I_buy = (prescriptions[_prescrId].purchase_cooldown+prescriptions[_prescrId].last_purchase < now);
-        time_to_expiration = prescriptions[_prescrId].expiration - now;
+        
+        // this gives the wrong number of days, looking for solution
+        time_to_expiration = prescriptions[_prescrId].expiration - now / 24 / 24 / 60;
         status = prescriptions[_prescrId].status;
 
         //return (prescriptions[_prescrId].drug, prescriptions[_prescrId].quantity, prescriptions[_prescrId].maxclaim, (prescriptions[_prescrId].purchase_cooldown+prescriptions[_prescrId].last_purchase < now), prescriptions[_prescrId].expiration - now, prescriptions[_prescrId].status);
@@ -118,7 +164,6 @@ contract bitpharma {
     }
 
     //function gettimestamp() external view returns(uint) {
-    //    return now;
+    //    return now / 1 days;
     //}
-
 }
