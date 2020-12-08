e5bd86faaff6e89224b00ccca212fd163c393919
AntoBcc <72571923+AntoBcc@users.noreply.github.com>
2020-12-08 16:09:29 +0100

    check for duplicate active prescriptions

--- Contract/prescriptions.sol
+++ Contract/prescriptions.sol
@@ -20,8 +20,6 @@ contract bitpharma {
 
 
     struct prescription {
-        //N.B: solidity  does not allow for string comparison, need to make sure all strings parameters are lowercased
-        //we could import this contract to lowercase: https://gist.github.com/ottodevs/c43d0a8b4b891ac2da675f825b1d1dbf
         string drug;
         uint quantity;  //total quantity of the drug claimable before prescription expires;
         uint quantity_claimed; //quantity claimed by user in a single purchase ??
@@ -40,16 +38,19 @@ contract bitpharma {
     mapping(address => mapping(address => bool)) patient_readers; //addresses who can access patient information
 
 
-    function set_whitelist_address(address _wl_address) external { //BitPharma can always update the address of the whitelist contract
+    function set_whitelist_address(address _wl_address) external { 
+        //BitPharma can always update the address of the whitelist contract
         require(msg.sender == bitpharma_manager, "Only BitPharma Manager can update the whitelist address");
         whitelist_address = _wl_address;
     }
 
-    function patient_add_reader(address _reader) external { //patients can allow new addresses to access their information (usually doctors)
+    function patient_add_reader(address _reader) external { 
+        //patients can allow new addresses to access their information (usually doctors)
         patient_readers[msg.sender][_reader] = true;
     }
 
-    function patient_remove_reader(address _reader) external { //patients can remove addresses the access to their information (usually doctors)
+    function patient_remove_reader(address _reader) external { 
+        //patients can remove addresses the access to their information (usually doctors)
         patient_readers[msg.sender][_reader] = false;
     }
 
@@ -58,13 +59,15 @@ contract bitpharma {
         //require(_patient == patient, "Please indicate a valid patient!");
         require(_daysToExpiration < 90, "You can't issue prescriptions for such long period of time");
         require( _purchaseCooldown*(_quantity/_maxclaim) <= _daysToExpiration, "Something is wrong please check...");
+        require(check_duplicate_prescriptions(_patient,_drug) == false, "The patient already has an active prescription for this drug");
         uint id = prescriptions.push(prescription(_drug, _quantity, 0, _maxclaim, _purchaseCooldown * 1 days, 0, now + _daysToExpiration * 1 days , 0, msg.sender)) - 1;
         prescription_to_patient[id] = _patient;
         active_prescriptions[_patient]++;
         emit Prescribed(_drug,_quantity,_maxclaim,_purchaseCooldown,_daysToExpiration,_patient);
     }
 
-    function patient_purchasing(uint _prescrId, uint _quantity) external {  //patient can confirm the purchase of a prescription in the array
+    function patient_purchasing(uint _prescrId, uint _quantity) external { 
+        //patient can confirm the purchase of a prescription in the array
         require(msg.sender == prescription_to_patient[_prescrId], "You don't have the right to access this prescription!");
         require(prescriptions[_prescrId].status==0, "You can't confirm purchase for this item!");
         require(prescriptions[_prescrId].maxclaim>=_quantity, "You can't buy this much in a single purchase");
@@ -80,7 +83,8 @@ contract bitpharma {
         }
     }
 
-    function close_transaction(uint _prescrId, uint _quantity) external {  //pharmacies can close transactions after patient has confirmed purchase
+    function close_transaction(uint _prescrId, uint _quantity) external {  
+        //pharmacies can close transactions after patient has confirmed purchase
         require(bitpharma_wl(whitelist_address).is_pharmacy(msg.sender), "Only pharmacies can close transactions!");
         require(prescriptions[_prescrId].status==1, "This transaction can't be confirmed!");
         require(prescriptions[_prescrId].quantity_claimed==_quantity, "Agree with the patient on the quantity claimed");
@@ -100,7 +104,8 @@ contract bitpharma {
     }
 
 
-    function my_prescriptions() external view returns(uint[] memory list_of_myPrescriptions_Ids) {  //return all active prescriptions IDs of a patient
+    function my_prescriptions() external view returns(uint[] memory list_of_myPrescriptions_Ids) {  
+        //return all active prescriptions IDs of a patient
         //require(msg.sender==patient, "You can't access prescriptions!");
         uint[] memory result = new uint[](active_prescriptions[msg.sender]);
         uint counter = 0;
@@ -111,17 +116,12 @@ contract bitpharma {
             }
         }
         list_of_myPrescriptions_Ids = result;
-        //return result;
     }
 
-    // TO ADD, when we have multiple doctors:
-    // not all doctors can be allowed to access a random patient's prescriptions (that is not respecting their privacy)
-    // Only doctors that are prescribing something to them should be able to do this if they want to.
-    // Actually, we need to check because I am not sure whether explicit consent from the patient is needed.
-
+  
     function patient_prescriptions(address _patient) external view  returns(uint[] memory list_of_Prescriptions_Ids) {
         //require(bitpharma_wl(whitelist_address).is_doctor(msg.sender), "You can't access prescriptions!");
-        require(patient_readers[_patient][msg.sender], "You can't access prescriptions!"); //anyone who is a reader for that patient can access information
+        require(patient_readers[_patient][msg.sender], "You can't access prescriptions!"); 
         uint[] memory result = new uint[](active_prescriptions[_patient]);
         uint counter = 0;
         for (uint i = 0; i < prescriptions.length; i++) {
@@ -133,20 +133,30 @@ contract bitpharma {
         list_of_Prescriptions_Ids = result;
     }
 
-    function check_prescriptions(address _patient, string calldata _drug) external view  returns(bool currently_prescripted, bool previously_prescripted) {
+    //function check_prescriptions(address _patient, string calldata _drug) external view  returns(bool currently_prescripted, bool previously_prescripted) {
         //require(bitpharma_wl(whitelist_address).is_doctor(msg.sender), "You can't access prescriptions!");
-        require(patient_readers[_patient][msg.sender], "You can't access prescriptions!"); //anyone who is a reader for that patient can access information
+        //require(patient_readers[_patient][msg.sender], "You can't access prescriptions!"); //anyone who is a reader for that patient can access information
         //uint[] memory result = new uint[](active_prescriptions[_patient]);
-        currently_prescripted = false;
-        previously_prescripted = false;
+        //currently_prescripted = false;
+        //previously_prescripted = false;
         // note: solidity does not allow for string comparison, but it does compare hashes (everything needs to be lowercased!)
+        //for (uint i = 0; i < prescriptions.length; i++) {
+        //    if (prescription_to_patient[i] == _patient  &&  keccak256(bytes(prescriptions[i].drug)) == keccak256(bytes(_drug))) {
+        //        if (prescriptions[i].status<2) currently_prescripted = true;
+        //        else previously_prescripted = true;
+        //    }
+        //}
+      //return(currently_prescripted, previously_prescripted);
+    //}
+    
+
+    function check_duplicate_prescriptions(address _patient, string calldata _drug) internal view returns(bool currently_prescribed) {
+        currently_prescribed = false;
         for (uint i = 0; i < prescriptions.length; i++) {
             if (prescription_to_patient[i] == _patient  &&  keccak256(bytes(prescriptions[i].drug)) == keccak256(bytes(_drug))) {
-                if (prescriptions[i].status<2) currently_prescripted = true;
-                else previously_prescripted = true;
+                if (prescriptions[i].status<2) currently_prescribed = true;
             }
         }
-        return(currently_prescripted, previously_prescripted);
     }
 
 
@@ -157,28 +167,8 @@ contract bitpharma {
         quantity_left = prescriptions[_prescrId].quantity;
         max_claim = prescriptions[_prescrId].maxclaim;
         can_I_buy = (prescriptions[_prescrId].purchase_cooldown+prescriptions[_prescrId].last_purchase < now);
-        // this gives the wrong number of days, looking for solution
         days_to_expiration = ((prescriptions[_prescrId].expiration - now) / 86400);
         status = prescriptions[_prescrId].status;
-
-        //return (prescriptions[_prescrId].drug, prescriptions[_prescrId].quantity, prescriptions[_prescrId].maxclaim, (prescriptions[_prescrId].purchase_cooldown+prescriptions[_prescrId].last_purchase < now), prescriptions[_prescrId].expiration - now, prescriptions[_prescrId].status);
     }
 
-
-    //just to print stuff
-    //function getdoctors() external view returns(address) {
-        //return doctor;
-    //}
-
-    //function getpatient() external view returns(address) {
-        //return patient;
-    //}
-
-    //function getpharma() external view returns(address) {
-        //return pharmacy;
-    //}
-
-    //function gettimestamp() external view returns(uint) {
-    //    return now / 1 days;
-    //}
 }
